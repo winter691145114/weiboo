2bcbe9d374bb2559dacfa32951b392ef0a59adc6
vagrant <vagrant@homestead>
2019-02-08 12:19:39 +0000

    updated database configuration

--- config/database.php
+++ config/database.php
@@ -67,6 +67,7 @@ return [
             'password' => $db_config['password'],
             'charset'  => 'utf8',
             'prefix'   => '',
+            'prefix_indexes' => true,
             'schema'   => 'public',
             'sslmode'  => 'prefer',
         ],
