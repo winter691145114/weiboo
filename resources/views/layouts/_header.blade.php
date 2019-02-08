049a12069da1817fe8a94c0dc7eb20e09147fada
vagrant <vagrant@homestead>
2019-02-08 02:04:51 +0000

    增加CSRF验证

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,13 +1,9 @@
-<header class="navbar navbar-fixed-top navbar-inverse">
-	<div class="container">
-		<div class="col-md-offset-1 col-md-10">
-			<a href="{{ route('home') }}" id="logo">Sample App</a>
-			<nav>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="{{ route('help') }}">帮助</a></li>
-					<li><a href="#">登录</a></li>
-				</ul>
-			</nav>
-	  	</div>
-	</div>
-</header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container ">
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+      <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+    </ul>
+  </div>
+</nav>
